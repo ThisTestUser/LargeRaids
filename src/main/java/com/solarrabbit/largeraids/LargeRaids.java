4484e9ca0073f75852e8a73a56ac4d408b1e5a46
ThisTestUser <ThisTestUser@users.noreply.github.com>
2025-09-09 13:02:20 +0000

    Downgrade to 1.20.2

--- src/main/java/com/solarrabbit/largeraids/LargeRaids.java
+++ src/main/java/com/solarrabbit/largeraids/LargeRaids.java
@@ -55,7 +55,7 @@ public final class LargeRaids extends JavaPlugin {
     private TriggerManager triggerManager;
     private VillageManager villageManager;
 
-    private static final String VERSION = "1.20.4";
+    private static final String VERSION = "1.20.2";
 
     @Override
     public void onEnable() {
